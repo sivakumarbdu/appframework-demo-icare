1a1c2b7a1afbf29d3fda27da0fe0d9e4c42f0e1f
Sivakumar V <sivakumarv@spritle.com>
2014-06-10 17:15:57 +0530

    load hospital and home controllers

--- www/app.js
+++ www/app.js
@@ -1,6 +1,6 @@
 var app = new $.mvc.app();
-app.loadControllers(["hello","hosptals","doctors","services"]); //You can pass in array or a string.  You do not need to reference the .js extension.
-  
-app.ready(function(){
-    // $.mvc.route("hello/");
+app.loadControllers(["hospitals","home"]); //You can pass in array or a string.  You do not need to reference the .js extension.
+ 
+ app.ready(function(){  
+   	$.mvc.route("home/index");
 });
